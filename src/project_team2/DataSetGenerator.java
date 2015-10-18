99ea886aabaf0cca16853e93e1f66f214b029644
Kilho <catharsistk@gmail.com>
2015-10-18 17:37:22 +0900

    Generate DataSetGenerator and Feature interface

---
+++ src/project_team2/DataSetGenerator.java
@@ -0,0 +1,36 @@
+package project_team2;
+
+import dbConnection.DBConn;
+import dbConnection.DBReader;
+import operator.ReadWriteInstances;
+import structure.log.BasicLog;
+import structure.log.motion.AccelerometerLog;
+import weka.core.Attribute;
+import weka.core.DenseInstance;
+import weka.core.Instances;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+
+/**
+ * Created by Ethan on 2015-09-23.
+ */
+public interface DataSetGenerator {
+
+    public ArrayList<String> getTableNames();
+
+    public Instances transformToInstances(HashMap<Integer, Feature> dataSet);
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////    Should be included!     //////////////////////////////////
+    public HashMap<Integer, Feature> generateDataSet(boolean test);
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public Feature generateFeature(HashMap<String, ArrayList<BasicLog>> userLogs, String label);
+
+    public Feature generateFeature_batchProcess(String tableName, int profileId, String label, boolean test);
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+}
---
+++ src/project_team2/Feature.java
@@ -0,0 +1,18 @@
+package project_team2;
+
+import java.lang.reflect.Field;
+
+/**
+ * Created by Ethan on 2015-09-24.
+ */
+public interface Feature {
+
+    public Field[] getNumericAttributes();
+
+    public Field[] getNominalAttributes();
+
+    public void setValues_Accelerometer(String dataType, double[] values);
+
+    public void setLabel(String label);
+
+}
